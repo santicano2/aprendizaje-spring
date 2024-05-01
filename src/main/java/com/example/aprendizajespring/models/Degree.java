d1f5d0d07eb43f8f6b27b98719902516b7285d7f
Santi <canosantiago404@gmail.com>
2024-05-01 10:42:28 -0300

    añadido config de mysql para crear bd automaticamente

--- src/main/java/com/example/aprendizajespring/models/Degree.java
+++ src/main/java/com/example/aprendizajespring/models/Degree.java
@@ -6,6 +6,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
 public class Degree {
 	private int id;
 	private String name;
@@ -15,9 +18,6 @@ public class Degree {
 
 	@Min(3)
 	private int year;
-
-	public Degree() {
-	}
 
 	public Degree(int id, String name, String institution, int year) {
 		this.id = id;
@@ -29,38 +29,6 @@ public class Degree {
 	public Degree(String name, String institution, int year) {
 		this.name = name;
 		this.institution = institution;
-		this.year = year;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getInstitution() {
-		return institution;
-	}
-
-	public void setInstitution(String institution) {
-		this.institution = institution;
-	}
-
-	public int getYear() {
-		return year;
-	}
-
-	public void setYear(int year) {
 		this.year = year;
 	}
 
